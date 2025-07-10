0059d50bd29975d293a23ad97774779358f83c8f
chabuuuu <haphuthinh332004@gmail.com>
2025-07-10 09:54:54 +0700

    custom(ev): color

---
+++ build-and-push-docker-hub.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Exit ngay lập tức nếu có bất kỳ lệnh nào lỗi
+set -e
+
+# Nhập các biến cần thiết
+# echo "Nhập tên người dùng Docker Hub:"
+# read DOCKER_USERNAME
+# echo "Nhập tên repository trên Docker Hub:"
+# read REPOSITORY
+echo "Nhập tag cho image (ví dụ: latest hoặc v1.0.0):"
+read TAG
+
+# Tên đầy đủ của image
+IMAGE_NAME="hieukhnt2022/vmm-b2c-rag:$TAG"
+
+# # Đăng nhập vào Docker Hub
+# echo "Đăng nhập vào Docker Hub..."
+# docker login
+
+# Build Docker image
+echo "Building Docker image $IMAGE_NAME..."
+sudo docker build -t "$IMAGE_NAME" .
+
+# Push Docker image lên Docker Hub
+echo "Pushing Docker image to Docker Hub..."
+sudo docker push "$IMAGE_NAME"
+
+# Thông báo hoàn thành
+echo "Docker image đã được push thành công: $IMAGE_NAME"
